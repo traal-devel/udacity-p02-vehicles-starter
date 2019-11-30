248081f3759a7973f620e2b94827f7f3d7e14d8d
devel@traal.ch <devel@traal.ch>
2019-11-30 20:47:47 +0100

    feat: Enable eureka client.

--- pricing-service/src/main/java/ch/traal/pricing/PricingServiceApplication.java
+++ pricing-service/src/main/java/ch/traal/pricing/PricingServiceApplication.java
@@ -2,16 +2,17 @@ package ch.traal.pricing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Creates a Spring Boot Application to run the Pricing Service. 
  * 
- * TODO: Convert the application from a REST API to a microservice.
+ * DONE: Convert the application from a REST API to a microservice.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PricingServiceApplication {
 
-  
   /* methods */
   public static void main(String[] args) {
     SpringApplication.run(PricingServiceApplication.class, args);
