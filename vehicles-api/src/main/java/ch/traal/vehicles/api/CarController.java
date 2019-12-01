35ebea1608fd69e5c21127e57c8a3d0f35126429
devel@traal.ch <devel@traal.ch>
2019-12-01 15:30:33 +0100

    feat: Implement the specified requirements (-> :TODO:)

--- vehicles-api/src/main/java/ch/traal/vehicles/api/CarController.java
+++ vehicles-api/src/main/java/ch/traal/vehicles/api/CarController.java
@@ -39,7 +39,16 @@ class CarController {
 
   
   /* constructors */
-  CarController(CarService carService, CarResourceAssembler assembler) {
+  /**
+   * Constructor for CarController.
+   * 
+   * @param carService - Service injected by spring
+   * @param assembler - Assembler instance injected by spring
+   */
+  CarController(
+      CarService carService, 
+      CarResourceAssembler assembler
+  ) {
     this.carService = carService;
     this.assembler = assembler;
   }
@@ -53,74 +62,114 @@ class CarController {
    */
   @GetMapping
   Resources<Resource<Car>> list() {
-    List<Resource<Car>> resources = carService.list().stream().map(assembler::toResource).collect(Collectors.toList());
-    return new Resources<>(resources, linkTo(methodOn(CarController.class).list()).withSelfRel());
+    List<Resource<Car>> resources = 
+            carService
+                .list()
+                .stream()
+                .map(assembler::toResource)
+                .collect(Collectors.toList());
+    
+    return new Resources<>(
+                  resources, 
+                  linkTo(
+                      methodOn(CarController.class).list()
+                  ).withSelfRel()
+               );
   }
 
   /**
    * Gets information of a specific car by ID.
    * 
-   * @param id the id number of the given vehicle
-   * @return all information for the requested vehicle
+   * @param id - The id number of the given vehicle
+   * @return All information for the requested vehicle
    */
   @GetMapping("/{id}")
-  Resource<Car> get(@PathVariable Long id) {
+  Resource<Car> get(
+      @PathVariable Long id
+  ) {
+    
     /**
-     * TODO: Use the `findById` method from the Car Service to get car information.
-     * TODO: Use the `assembler` on that car and return the resulting output. Update
-     * the first line as part of the above implementing.
+     * :DONE: Use the `findById` method from the Car Service to get car information.
+     * :DONE: Use the `assembler` on that car and return the resulting output. 
+     *    Update the first line as part of the above implementing. 
+     *    ---> :CHECK: jk, ??? -> I think, I've done this correctly...  
      */
-    return assembler.toResource(new Car());
+    Car car = this.carService.findById(id);
+    return assembler.toResource(car);
+    
   }
 
   /**
    * Posts information to create a new vehicle in the system.
    * 
-   * @param car A new vehicle to add to the system.
+   * @param car - A new vehicle to add to the system.
    * @return response that the new vehicle was added to the system
    * @throws URISyntaxException if the request contains invalid fields or syntax
    */
   @PostMapping
-  ResponseEntity<?> post(@Valid @RequestBody Car car) throws URISyntaxException {
+  ResponseEntity<?> post(
+      @Valid @RequestBody Car car
+  ) throws URISyntaxException {
+  
     /**
-     * TODO: Use the `save` method from the Car Service to save the input car. TODO:
-     * Use the `assembler` on that saved car and return as part of the response.
-     * Update the first line as part of the above implementing.
+     * :DONE: Use the `save` method from the Car Service to save the input car. 
+     * :DONE: Use the `assembler` on that saved car and return as part of the response.
+     *   Update the first line as part of the above implementing.
      */
-    Resource<Car> resource = assembler.toResource(new Car());
-    return ResponseEntity.created(new URI(resource.getId().expand().getHref())).body(resource);
+    Car carDB = this.carService.save(car);
+    Resource<Car> resource = assembler.toResource(carDB);
+    
+    return ResponseEntity
+                .created(
+                  new URI(resource.getId().expand().getHref())
+                ).body(resource);
+    
   }
 
   /**
    * Updates the information of a vehicle in the system.
    * 
-   * @param id  The ID number for which to update vehicle information.
-   * @param car The updated information about the related vehicle.
+   * @param id - The ID number for which to update vehicle information.
+   * @param car - The updated information about the related vehicle.
    * @return response that the vehicle was updated in the system
    */
   @PutMapping("/{id}")
-  ResponseEntity<?> put(@PathVariable Long id, @Valid @RequestBody Car car) {
+  ResponseEntity<?> put(
+      @PathVariable Long id, 
+      @Valid @RequestBody Car car
+  ) {
+  
     /**
-     * TODO: Set the id of the input car object to the `id` input. TODO: Save the
-     * car using the `save` method from the Car service TODO: Use the `assembler` on
-     * that updated car and return as part of the response. Update the first line as
-     * part of the above implementing.
+     * :DONE: Set the id of the input car object to the `id` input. 
+     * :DONE: Save the car using the `save` method from the Car service 
+     * TODO: Use the `assembler` on that updated car and return as part of 
+     *   the response. Update the first line as part of the above implementing.
      */
-    Resource<Car> resource = assembler.toResource(new Car());
+    car.setId(id);
+    Car carDB = this.carService.save(car);
+    
+    Resource<Car> resource = assembler.toResource(carDB);
     return ResponseEntity.ok(resource);
+    
   }
 
   /**
    * Removes a vehicle from the system.
    * 
-   * @param id The ID number of the vehicle to remove.
+   * @param id - The ID number of the vehicle to remove.
    * @return response that the related vehicle is no longer in the system
    */
   @DeleteMapping("/{id}")
-  ResponseEntity<?> delete(@PathVariable Long id) {
+  ResponseEntity<?> delete(
+      @PathVariable Long id
+  ) {
+    
     /**
-     * TODO: Use the Car Service to delete the requested vehicle.
+     * :DONE: Use the Car Service to delete the requested vehicle.
      */
+    this.carService.delete(id);
+    
     return ResponseEntity.noContent().build();
+    
   }
 }
