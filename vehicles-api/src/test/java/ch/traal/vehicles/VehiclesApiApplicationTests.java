739a66a7be82c47fb625d28a3b5a1ee1ce0c5751
devel@traal.ch <devel@traal.ch>
2019-12-01 15:11:24 +0100

    chore: Add basic tests to check if WebClients can be injected by spring.

--- vehicles-api/src/test/java/ch/traal/vehicles/VehiclesApiApplicationTests.java
+++ vehicles-api/src/test/java/ch/traal/vehicles/VehiclesApiApplicationTests.java
@@ -1,19 +1,45 @@
 package ch.traal.vehicles;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import ch.traal.vehicles.client.maps.MapsClient;
+import ch.traal.vehicles.client.prices.PriceClient;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class VehiclesApiApplicationTests {
 
   
+  /* member variables */
+  @Autowired
+  private PriceClient wcPrice;
+  
+  @Autowired
+  private MapsClient  wcMaps;
+  
+  
+  /* constructors */
+  
+  
   /* methods */
   @Test
-  public void contextLoads() {
+  public void testNotNull() {
     
+    assertNotNull(this.wcPrice);
+    assertNotNull(this.wcMaps);
+    
+  }
+  
+  @Test
+  public void contextLoads() {
+    assertTrue(true);
   }
 
 }
