1eba448cfcd66144d062149e0bb24db1b8686709
devel@traal.ch <devel@traal.ch>
2019-12-02 14:40:33 +0100

    feat: Convert to a microservice with Spring Data REST.
    Note: Without the need to explicitly include code for the Controller or Service

--- pricing-service/src/main/java/ch/traal/pricing/domain/price/PriceRepository.java
+++ pricing-service/src/main/java/ch/traal/pricing/domain/price/PriceRepository.java
@@ -1,8 +1,15 @@
 package ch.traal.pricing.domain.price;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
 
-@Repository
-public class PriceRepository {
+/**
+ * 
+ * :DONE: The Pricing Service API is converted to a microservice with 
+ * Spring Data REST, without the need to explicitly include code for the 
+ * Controller or Service.
+ * 
+ * @author traal-devel
+ */
+public interface PriceRepository extends CrudRepository<Price, Long>{
 
 }
