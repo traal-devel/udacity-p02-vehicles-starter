092d73ec047a1fe89582264e017cb5546e37160d
devel@traal.ch <devel@traal.ch>
2019-12-02 14:41:28 +0100

    style: Update java doc.

--- vehicles-api/src/main/java/ch/traal/vehicles/api/CarController.java
+++ vehicles-api/src/main/java/ch/traal/vehicles/api/CarController.java
@@ -142,7 +142,7 @@ class CarController {
     /**
      * :DONE: Set the id of the input car object to the `id` input. 
      * :DONE: Save the car using the `save` method from the Car service 
-     * TODO: Use the `assembler` on that updated car and return as part of 
+     * :DONE: Use the `assembler` on that updated car and return as part of 
      *   the response. Update the first line as part of the above implementing.
      */
     car.setId(id);
