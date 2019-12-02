800667c4e310c8637370bbd60169b0aaa9f49633
devel@traal.ch <devel@traal.ch>
2019-12-02 23:10:57 +0100

    refactor: Make public.

--- pricing-service/src/test/java/ch/traal/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/ch/traal/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,31 @@
 package ch.traal.pricing;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class PricingServiceApplicationTests {
+@Import(PriceConfig.class)
+public class PricingServiceApplicationTests {
 
+  
+  /* member variables */
+  
+  
+  /* constructors */
+  public PricingServiceApplicationTests() {
+    super();
+  }
+  
+  /* methods */
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+	  assertTrue(true);
 	}
 
 }
