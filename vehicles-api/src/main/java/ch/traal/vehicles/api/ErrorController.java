06a695cb890fcd7e1f23e1f448441cd08d3d9262
devel@traal.ch <devel@traal.ch>
2019-12-03 16:05:23 +0100

    style: Change java code format.

--- vehicles-api/src/main/java/ch/traal/vehicles/api/ErrorController.java
+++ vehicles-api/src/main/java/ch/traal/vehicles/api/ErrorController.java
@@ -39,7 +39,8 @@ public class ErrorController extends ResponseEntityExceptionHandler {
         ex.getBindingResult()
             .getFieldErrors()
             .stream()
-            .map(error -> error.getField() + ": " + error.getDefaultMessage()
+            .map(
+              error -> error.getField() + ": " + error.getDefaultMessage()
             ).collect(Collectors.toList());
 
     ApiError apiError = new ApiError(DEFAULT_VALIDATION_FAILED_MESSAGE, errors);
